397c641f8907e5f381bfa23e0ed27e40c759bae0
A13-Cloud <avik.ghukasyan13@gmail.com>
2024-01-02 19:55:05 +0400

    feat(path): add loading image files for react icon

--- learning/react_essentials__components_JSX_props_state_more/src/App.jsx
+++ learning/react_essentials__components_JSX_props_state_more/src/App.jsx
@@ -1,5 +1,6 @@
-function App() {
+import reactImg from "./assets/react-core-concepts.png";
 
+function App() {
   const reactDescriptions = ["Fundamental", "Crucial", "Core"];
   function genRandomInt (max) {
     return Math.floor(Math.random() * (max + 1));
@@ -10,7 +11,7 @@ function App() {
 
     return (
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
+        <img src={reactImg} alt="Stylized atom"/>
         <h1>React Essentials</h1>
         <p>
           {description} React concepts you will need for almost any app you are
