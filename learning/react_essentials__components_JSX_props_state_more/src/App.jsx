dcb4ddc0da807fca7c0dc3abddc235c12a53f15a
A13-Cloud <avik.ghukasyan13@gmail.com>
2024-01-02 16:23:37 +0400

    feat(header): add custom component in app component

--- learning/react_essentials__components_JSX_props_state_more/src/App.jsx
+++ learning/react_essentials__components_JSX_props_state_more/src/App.jsx
@@ -1,14 +1,21 @@
 function App() {
-  return (
-    <div>
+
+  function Header() {
+    return (
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
         <h1>React Essentials</h1>
         <p>
           Fundamental React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
+    );
+  }
+
+  return (
+    <div>
+      <Header/>
       <main>
         <h2>Time to get started!</h2>
       </main>
