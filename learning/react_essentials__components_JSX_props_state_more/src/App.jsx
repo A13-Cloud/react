f777df3603d71b351c1eb723ada3d402f6cbbf2e
A13-Cloud <avik.ghukasyan13@gmail.com>
2024-01-07 20:49:27 +0400

    feat(list_data): add outputting list data dynamically

--- learning/react_essentials__components_JSX_props_state_more/src/App.jsx
+++ learning/react_essentials__components_JSX_props_state_more/src/App.jsx
@@ -35,19 +35,28 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}/>
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            {CORE_CONCEPTS.map(conceptItem => <CoreConcept key={conceptItem.title} {...conceptItem}/>)}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => onSelectHandler("components")}>Components</TabButton>
-            <TabButton onSelect={() => onSelectHandler("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => onSelectHandler("props")}>Props</TabButton>
-            <TabButton onSelect={() => onSelectHandler("state")}>State</TabButton>
+            <TabButton
+              selectedTab={selectedTopic === "components"}
+              onSelect={() => onSelectHandler("components")}
+            >Components</TabButton>
+            <TabButton
+              selectedTab={selectedTopic === "jsx"}
+              onSelect={() => onSelectHandler("jsx")}
+            >JSX</TabButton>
+            <TabButton
+              selectedTab={selectedTopic === "props"}
+              onSelect={() => onSelectHandler("props")}
+            >Props</TabButton>
+            <TabButton
+              selectedTab={selectedTopic === "state"}
+              onSelect={() => onSelectHandler("state")}
+            >State</TabButton>
           </menu>
           {tabContent}
         </section>
