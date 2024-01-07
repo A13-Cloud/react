263cc30e17da410e760368398ce303db14b13014
Avik013 <gukasyan.spa@gmail.com>
2024-01-07 19:23:46 +0400

    feat(condition): add rendering content conditionally with data

--- learning/react_essentials__components_JSX_props_state_more/src/App.jsx
+++ learning/react_essentials__components_JSX_props_state_more/src/App.jsx
@@ -1,12 +1,31 @@
 import { CORE_CONCEPTS } from "./data";
+import { EXAMPLES } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
 import TabButton from "./components/TabButton";
+import {useState} from "react";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function onSelectHandler (selectedButton) {
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please click a tab button</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -20,18 +39,17 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[1]}/>
             <CoreConcept {...CORE_CONCEPTS[2]}/>
             <CoreConcept {...CORE_CONCEPTS[3]}/>
-
-            
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => onSelectHandler("component")}>Component</TabButton>
+            <TabButton onSelect={() => onSelectHandler("components")}>Components</TabButton>
             <TabButton onSelect={() => onSelectHandler("jsx")}>JSX</TabButton>
             <TabButton onSelect={() => onSelectHandler("props")}>Props</TabButton>
             <TabButton onSelect={() => onSelectHandler("state")}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
