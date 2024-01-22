408bfd542e2b241a03c7dae2c994c7e3c3acef3a
A13-Cloud <gukasyan.spa@gmail.com>
2024-01-22 19:59:22 +0400

    feat(player): add updating state based on old state correctly

--- learning/react_essentials__deep_dive/src/component/Player.jsx
+++ learning/react_essentials__deep_dive/src/component/Player.jsx
@@ -1,13 +1,27 @@
+import {useState} from "react";
 
 export default function Player ({name, symbol}) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick () {
+    setIsEditing((isEditing) => !isEditing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name}/>
+    btnCaption = "Save";
+  }
 
   return (
     <li>
-      <soan className="player">
-        <span className="player-name">{name}</span>
-        <span className="player-symbol">{symbol}</span>
-      </soan>
-      <button>Edit</button>
+      <span className="player">
+        {playerName}
+        {<span className="player-symbol">{symbol}</span>}
+      </span>
+      <button onClick={handleEditClick}>{btnCaption}</button>
     </li>
   )
 }
