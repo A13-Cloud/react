a0aa9aade74be6221f78ac34917194e2c3520875
A13-Cloud <avik.ghukasyan13@gmail.com>
2024-02-08 02:54:28 +0400

    feat(app): add an alternative to lifting state up

--- learning/react_essentials__deep_dive/src/App.jsx
+++ learning/react_essentials__deep_dive/src/App.jsx
@@ -23,10 +23,11 @@ function deriveActivePlayer (gameTurns) {
 }
 
 function App() {
+  const [players, setPlayers] = useState({ X: "Player 1", Y: "Player 2" });
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer =  deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for (let turn of gameTurns) {
     const {square, player} = turn;
@@ -46,7 +47,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -67,14 +68,37 @@ function App() {
     });
   }
 
+  function handleRestart () {
+    setGameTurns([]);
+  }
+
+  function handlePlayerNameChange (symbol, newName) {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        symbol: newName
+      };
+    });
+  }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player" >
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"}/>
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
+          />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner}/>}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
       </div>
       <Log turns={gameTurns}/>
