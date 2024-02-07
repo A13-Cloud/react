90afc365b8467ad0b5a61e0b8af3b807588ef924
A13-Cloud <avik.ghukasyan13@gmail.com>
2024-02-07 21:20:35 +0400

    feat(game-over): add the game over screen and checking for a draw

--- learning/react_essentials__deep_dive/src/App.jsx
+++ learning/react_essentials__deep_dive/src/App.jsx
@@ -4,6 +4,7 @@ import Player from "./component/Player.jsx";
 import GameBoard from "./component/GameBoard.jsx";
 import Log from "./component/Log.jsx";
 import { WINING_COMBINATIONS } from "./wining-cambiations.js";
+import GameOver from "./component/GameOver.jsx";
 
 const initialGameBoard = [
   [null, null, null],
@@ -49,6 +50,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare (rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
       const currentPlayer = deriveActivePlayer(prevTurns);
@@ -71,7 +74,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"}/>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"}/>
         </ol>
-        {winner && <p>You won! {winner}</p>}
+        {(winner || hasDraw) && <GameOver winner={winner}/>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
       </div>
       <Log turns={gameTurns}/>
