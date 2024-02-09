2c1746ef57c0ed8a2d9d2a9e1f93a41cbd293492
A13-Cloud <gukasyan.spa@gmail.com>
2024-02-09 22:20:54 +0400

    feat(header): add header component

--- projects/investment_calculator /src/App.jsx
+++ projects/investment_calculator /src/App.jsx
@@ -1,6 +1,18 @@
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <h1>React Investment Calculator</h1>
+      <div id="header">
+        Header
+        <img/>
+        
+        <h1>h1</h1>
+      </div>
+      <div id="#user-input">
+
+      </div>
+
+    </>
   )
 }
 
