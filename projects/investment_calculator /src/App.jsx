1fb005a107efeb36c86e3e08c1f9e157bc27c0ae
A13-Cloud <gukasyan.spa@gmail.com>
2024-02-13 20:41:02 +0400

    feat(input): add user input component

--- projects/investment_calculator /src/App.jsx
+++ projects/investment_calculator /src/App.jsx
@@ -1,20 +1,9 @@
+import Header from "./components/Header.jsx";
+
+
 function App() {
   return (
-    <>
-      <h1>React Investment Calculator</h1>
-      <div id="header">
-        Header
-        <img/>
-
-
-        
-        <h1>h1</h1>
-      </div>
-      <div id="#user-input">
-
-      </div>
-
-    </>
+   <Header />
   )
 }
 
