e30b7a5dbcc619009c458478731215c7fd01666f
A13-Cloud <avik.ghukasyan13@gmail.com>
2024-03-03 00:21:03 +0400

    feat(player): add handles for player with states

--- learning/working-with-refs-portals/src/components/Player.jsx
+++ learning/working-with-refs-portals/src/components/Player.jsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
 
 export default function Player() {
-  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
 
   function handleChange (event) {
+    setSubmitted(false);
     setEnteredPlayerName(event.target.value);
   }
 
-  console.log(enteredPlayerName)
-
+  function handleClick () {
+    setSubmitted(true);
+  }
+  
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
       <p>
         <input type="text" onChange={handleChange}/>
-        <button>Set Name</button>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
