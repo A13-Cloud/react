d1ba9f7f7567638c6ba7f630ea2b2fb7f54e00fc
A13-Cloud <gukasyan.spa@gmail.com>
2024-03-11 19:44:20 +0400

    feat(timer_challenge): add reset functionality inside result modal

--- learning/working-with-refs-portals/src/components/ResultModal.jsx
+++ learning/working-with-refs-portals/src/components/ResultModal.jsx
@@ -1,24 +1,27 @@
 import { useRef, forwardRef, useImperativeHandle } from "react";
 
 const ResultModal = forwardRef(
-  function ResultModal ({result, targetTimer}, ref) {
+  function ResultModal ({targetTimer, remainingTime, onReset}, ref) {
     const imperativeDialogRef = useRef();
+
+    let userLost = remainingTime <= 0;
+    let formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
     useImperativeHandle(ref, () => {
       return {
         open() {
-          imperativeDialogRef.current.showModal();
+          imperativeDialogRef.current?.showModal();
         }
       };
     })
 
     return (
       <dialog ref={imperativeDialogRef} className="result-modal">
-        <h2>You {result}</h2>
+        {userLost && <h2>You Lost!</h2>}
         <p>The target time was <strong>{targetTimer} seconds</strong></p>
-        <p>You stopped the timer with <strong>X second left</strong></p>
+        <p>You stopped the timer with <strong>{formattedRemainingTime} second left</strong></p>
         <form method="dialog">
-          <button>Close</button>
+          <button onClick={onReset}>Close</button>
         </form>
       </dialog>
     );
