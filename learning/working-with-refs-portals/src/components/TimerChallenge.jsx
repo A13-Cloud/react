ae4350bd671fa0a8815dfdb97416d75e1e720043
A13-Cloud <gukasyan.spa@gmail.com>
2024-03-13 18:26:20 +0400

    feat(result_modal): add score when user lost inside result modal

--- learning/working-with-refs-portals/src/components/TimerChallenge.jsx
+++ learning/working-with-refs-portals/src/components/TimerChallenge.jsx
@@ -39,7 +39,7 @@ export default function TimerChallenge ({title, targetTime}) {
       />
       <section className="challenge">
         <h2>{title}</h2>
-        {timerIsActive && <p>You Lost!</p>}
+        {/*{timerIsActive && <p>You Lost!</p>}*/}
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
