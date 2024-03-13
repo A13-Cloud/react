2b288b3c3c8dbd3d1efeab6476be0630a616984d
A13-Cloud <avik.ghukasyan13@gmail.com>
2024-03-13 18:26:20 +0400

    feat(result_modal): add score when user lost inside result modal

--- learning/working-with-refs-portals/src/components/ResultModal.jsx
+++ learning/working-with-refs-portals/src/components/ResultModal.jsx
@@ -4,8 +4,9 @@ const ResultModal = forwardRef(
   function ResultModal ({targetTimer, remainingTime, onReset}, ref) {
     const imperativeDialogRef = useRef();
 
-    let userLost = remainingTime <= 0;
-    let formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTimer * 1000)) * 100);
 
     useImperativeHandle(ref, () => {
       return {
@@ -18,6 +19,7 @@ const ResultModal = forwardRef(
     return (
       <dialog ref={imperativeDialogRef} className="result-modal">
         {userLost && <h2>You Lost!</h2>}
+        {!userLost && <h2>Your Score {score}</h2>}
         <p>The target time was <strong>{targetTimer} seconds</strong></p>
         <p>You stopped the timer with <strong>{formattedRemainingTime} second left</strong></p>
         <form method="dialog">
