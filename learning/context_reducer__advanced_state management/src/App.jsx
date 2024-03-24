466ab247c5a46850e9143226e3c64accbc4a6311
A13-Cloud <avik.ghukasyan13@gmail.com>
2024-03-25 03:12:10 +0400

    feat(components): change inside of all components drilling props

--- learning/context_reducer__advanced_state management/src/App.jsx
+++ learning/context_reducer__advanced_state management/src/App.jsx
@@ -69,15 +69,13 @@ function App() {
 
   const ctxValue = {
     items: shoppingCart.items,
-    addItemToCart: handleUpdateCartItemQuantity
+    addItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity
   }
 
   return (
     <CartContext.Provider value={ctxValue}>
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
+      <Header />
       <Shop>
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
