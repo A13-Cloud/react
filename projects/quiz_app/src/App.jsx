b8422c528ced46a56d1951f96467e6b9c0d5ec03
A13-Cloud <avik.ghukasyan13@gmail.com>
2024-03-28 19:22:44 +0400

    feat(component): add header and quiz empty components

--- projects/quiz_app/src/App.jsx
+++ projects/quiz_app/src/App.jsx
@@ -1,3 +1,16 @@
-function App() {}
+import Header from "./components/Header.jsx";
+
+function App() {
+
+
+  return (
+    <>
+      <Header/>
+      <main>
+        {/*<Quiz/>*/}
+      </main>
+    </>
+  )
+}
 
 export default App;
