3f47c05e8934676084b4061d78689e59905328a4
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-05-06 17:56:49 +0400

    feat(ui): add input component of ui folder with some updates

--- projects/food_order_app/src/components/UI/Modal.jsx
+++ projects/food_order_app/src/components/UI/Modal.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 
 import { createPortal } from "react-dom";
 
-export default function Modal ({children, open, className = ""}) {
+export default function Modal ({children, open, onClose, className = ""}) {
   const modalRef = useRef();
 
   useEffect(() => {
@@ -17,8 +17,9 @@ export default function Modal ({children, open, className = ""}) {
 
   return createPortal(
     <dialog
-      className={`modal ${className}`}
       ref={modalRef}
+      onClose={onClose}
+      className={`modal ${className}`}
     >
       {children}
     </dialog>,
