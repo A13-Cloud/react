883cced5b03a56321706420122c19dcc9f2bec45
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-05-06 17:58:05 +0400

    feat(components): add cart item and checkout components

---
+++ projects/food_order_app/src/components/Checkout.jsx
@@ -0,0 +1,52 @@
+import { useContext } from "react";
+
+import Modal from "./UI/Modal.jsx";
+import CartContext from "../store/CartContext.jsx";
+import {currencyFormatter} from "../util/formatting.js";
+import Input from "./UI/Input.jsx";
+import Button from "./UI/Button.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
+
+
+export default function Checkout () {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
+  function handleCloseCart () {
+    userProgressCtx.hideCheckout();
+  }
+
+  return (
+    <Modal
+      onClose={handleCloseCart}
+      open={userProgressCtx.progress === "checkout"}
+    >
+      <form>
+        <h2>Checkout</h2>
+        <p>Total Amount: {currencyFormatter.format(totalCartItems)}</p>
+
+        <Input label="Full Name" type="text" id="full-name"/>
+        <Input label="Email Address" type="email" id="email"/>
+        <Input label="Street" type="text" id="street"/>
+
+        <div className="control-row">
+          <Input label="Postal Code" type="text" id="postal-code"/>
+          <Input label="City" type="text" id="city"/>
+        </div>
+
+        <p className="modal-actions">
+          <Button
+            textOnly
+            type="button"
+            onClick={handleCloseCart}
+          >Close</Button>
+          <Button>Submit Order</Button>
+        </p>
+      </form>
+    </Modal>
+  )
+}
